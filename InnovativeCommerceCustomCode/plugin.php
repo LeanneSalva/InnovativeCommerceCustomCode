2676bdfd02f1a7f20127c26f2b730a82d69f5ce3
Leanne Rose Salva <123197122+LeanneSalva@users.noreply.github.com>
2023-02-17 12:34:50 -0500

    Add files via upload

---
+++ InnovativeCommerceCustomCode/plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: InnovativeCommerceCustomCode
+Plugin URI: http: //www.InnovativeCommerceCustomCode.com
+Description: Create your of custom Javascript code with this easy-to-use plugin.
+Author: Leanne Salva
+Author URI: http://www.InnovativeCommerceCustomCode.com/
+*/
+
+add_action('wp_enqueue_scripts'); 
+add_action('wp_enqueue_scripts',, 'plugin_scripts');
+function plugin_scripts(){
+    wp_enqueue_script('InnovativeCommerceCustomCode', plugin_url('/scripts.js',__FILE__), array('jquery'), false, true);
+}
