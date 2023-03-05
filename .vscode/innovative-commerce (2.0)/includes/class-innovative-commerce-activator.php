928d2a6562570cb3ef21dfd3faafac1a138ef0af
Leanne Rose Salva <lrsg0706@gmail.com>
2023-03-05 13:21:05 -0500

    commit

---
+++ .vscode/innovative-commerce (2.0)/includes/class-innovative-commerce-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/LeanneSalva
+ * @since      1.0.0
+ *
+ * @package    Innovative_Commerce
+ * @subpackage Innovative_Commerce/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Innovative_Commerce
+ * @subpackage Innovative_Commerce/includes
+ * @author     Leanne R Salva <lrsg0706@gmail.com>
+ */
+class Innovative_Commerce_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ .vscode/innovative-commerce (2.0)/includes/class-innovative-commerce-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://github.com/LeanneSalva
+ * @since      1.0.0
+ *
+ * @package    Innovative_Commerce
+ * @subpackage Innovative_Commerce/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Innovative_Commerce
+ * @subpackage Innovative_Commerce/includes
+ * @author     Leanne R Salva <lrsg0706@gmail.com>
+ */
+class Innovative_Commerce_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
